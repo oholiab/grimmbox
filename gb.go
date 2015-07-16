71a15128763933a52d9f16d3b7d4de58b9a7d42d
Matt Carroll <mattc@yelp.com>
2015-07-16 20:24:02 +0100

    Made text box more workier

--- gb.go
+++ gb.go
@@ -4,8 +4,11 @@ import "fmt"
 import "os"
 import ioutil "io/ioutil"
 import ui "github.com/nsf/termbox-go"
+import "strings"
+import "strconv"
 
 var f, b = ui.ColorDefault, ui.ColorDefault
+var debug []string
 
 type Coord struct {
 	X int
@@ -13,20 +16,21 @@ type Coord struct {
 }
 
 type Box struct {
-	Title   string
-	Color   ui.Attribute
-	TL      Coord // top left
-	TR      Coord // top right
-	BL      Coord // bottom left
-	BR      Coord // bottom right
-	W       int
-	H       int
-	Content string
+	Title string
+	Color ui.Attribute
+	TL    Coord // top left
+	TR    Coord // top right
+	BL    Coord // bottom left
+	BR    Coord // bottom right
+	W     int
+	H     int
+	Text  string
 }
 
 func on_exit() {
 	fmt.Printf("thanks for playing\n")
 	ls()
+	fmt.Println("%v\n", strings.Join(debug, "||"))
 }
 
 func makeBox(title, content string, x, y, height, width int) Box {
@@ -41,7 +45,8 @@ func makeBox(title, content string, x, y, height, width int) Box {
 	box.BR.X = x + width - 1
 	box.BR.Y = y + height - 1
 	box.W = width
-	box.W = height
+	box.H = height
+	box.Text = content
 
 	return box
 }
@@ -62,30 +67,50 @@ func drawBoxCorners(box Box) {
 
 func drawBoxSides(box Box) {
 	for i := box.TL.X + 1; i < box.TR.X; i++ {
-		ui.SetCell(i, box.TL.Y, '-', f, b)
-		ui.SetCell(i, box.BL.Y, '-', f, b)
+		ui.SetCell(i, box.TL.Y, '─', f, b)
+		ui.SetCell(i, box.BL.Y, '─', f, b)
 	}
 	for j := box.TL.Y + 1; j < box.BL.Y; j++ {
-		ui.SetCell(box.TL.X, j, '|', f, b)
-		ui.SetCell(box.BR.X, j, '|', f, b)
+		ui.SetCell(box.TL.X, j, '│', f, b)
+		ui.SetCell(box.BR.X, j, '│', f, b)
 	}
 }
 
-//func boxText(text string, width, height int) ([][]byte, int) {
-//	buf = []byte(text)
-//	// lines times create slices of width, return them as an array of byte arrays
-//	// with remainder r
-//
-//	//Probably string array actually depending on what writeln does
-//}
+func boxText(text string, width, height int) ([]string, int) {
+	var lines []string
+	var sliceLength int
+	buf := []byte(text)
+	for len(buf) > 0 {
+		if width-2 < len(buf) {
+			sliceLength = width - 2
+		} else {
+			sliceLength = len(buf)
+		}
+		lines = append(lines, string(buf[0:sliceLength]))
+		if sliceLength == len(buf) {
+			break
+		} else {
+			buf = buf[sliceLength:]
+		}
+	}
+	rem := len(lines) - height
+	debug = append(debug, strings.Join(lines, " "))
+	debug = append(debug, strconv.FormatInt(int64(len(lines)), 10))
+	if rem > 0 {
+		return lines[:height], rem
+	} else {
+		return lines, rem
+	}
+}
 
 func drawBox(box Box) {
 	drawBoxCorners(box)
 	drawBoxSides(box)
 	writeln(box.Title, box.TL.X+1, box.TL.Y)
-	//for i, line := range boxText(box.Text, box.W, box.Height) {
-	//	writeln(line, box.TL.X+1, box.TL.Y+i)
-	//}
+	lines, _ := boxText(box.Text, box.W, box.H)
+	for i, line := range lines {
+		writeln(line, box.TL.X+1, box.TL.Y+1+i)
+	}
 }
 
 func writeln(s string, x, y int) {
@@ -105,7 +130,8 @@ func main() {
 
 	// ui.SetInputMode(ui.InputEsc) //default
 	w, h := ui.Size()
-	box := makeBox("grimmwa.re", "some text", 1, 2, h-3, w-3)
+	ridonk := "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+	box := makeBox("grimmwa.re", ridonk, 1, 2, h-3, w-3)
 	writeln(":PRESS C-q TO EXIT", 0, 0)
 	drawBox(box)
 	ui.Flush()
