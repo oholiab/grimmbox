43926eb93b7c943a3068e226c3333f0d82e9ed39
Matt Carroll <mattc@yelp.com>
2015-07-16 20:47:33 +0100

    Done made it orange :)

--- gb.go
+++ gb.go
@@ -7,7 +7,8 @@ import ui "github.com/nsf/termbox-go"
 import "strings"
 import "strconv"
 
-var f, b = ui.ColorDefault, ui.ColorDefault
+var orange = ui.Attribute(0x005f)
+var f, d = orange, ui.ColorDefault
 var debug []string
 
 type Coord struct {
@@ -59,20 +60,20 @@ func ls() {
 }
 
 func drawBoxCorners(box Box) {
-	ui.SetCell(box.TL.X, box.TL.Y, '┌', f, b)
-	ui.SetCell(box.TR.X, box.TR.Y, '┐', f, b)
-	ui.SetCell(box.BL.X, box.BL.Y, '└', f, b)
-	ui.SetCell(box.BR.X, box.BR.Y, '┘', f, b)
+	ui.SetCell(box.TL.X, box.TL.Y, '┌', f, d)
+	ui.SetCell(box.TR.X, box.TR.Y, '┐', f, d)
+	ui.SetCell(box.BL.X, box.BL.Y, '└', f, d)
+	ui.SetCell(box.BR.X, box.BR.Y, '┘', f, d)
 }
 
 func drawBoxSides(box Box) {
 	for i := box.TL.X + 1; i < box.TR.X; i++ {
-		ui.SetCell(i, box.TL.Y, '─', f, b)
-		ui.SetCell(i, box.BL.Y, '─', f, b)
+		ui.SetCell(i, box.TL.Y, '─', f, d)
+		ui.SetCell(i, box.BL.Y, '─', f, d)
 	}
 	for j := box.TL.Y + 1; j < box.BL.Y; j++ {
-		ui.SetCell(box.TL.X, j, '│', f, b)
-		ui.SetCell(box.BR.X, j, '│', f, b)
+		ui.SetCell(box.TL.X, j, '│', f, d)
+		ui.SetCell(box.BR.X, j, '│', f, d)
 	}
 }
 
@@ -115,7 +116,7 @@ func drawBox(box Box) {
 
 func writeln(s string, x, y int) {
 	for i, c := range s {
-		ui.SetCell(x+i, y, c, f, b)
+		ui.SetCell(x+i, y, c, d, d)
 	}
 }
 
@@ -128,9 +129,10 @@ func main() {
 
 	defer ui.Close()
 
+	ui.SetOutputMode(ui.Output256)
 	// ui.SetInputMode(ui.InputEsc) //default
 	w, h := ui.Size()
-	ridonk := "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+	ridonk := " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae mollis eros. Aenean at nisl laoreet, porta risus in, placerat dolor. Etiam massa augue, consequat efficitur augue pellentesque, viverra facilisis ex. Curabitur tristique nulla eget urna semper vestibulum. Cras eget eros ex. Vestibulum at ante leo. Quisque faucibus vulputate lacus vitae porta. Vivamus nulla erat, elementum id pulvinar sed, dapibus non diam. Nulla varius finibus est, ut laoreet enim venenatis id. Aliquam suscipit lectus neque, sit amet sollicitudin dui accumsan nec. Nulla enim felis, molestie at facilisis vitae, elementum et mauris. Aenean sit amet finibus ligula, ut luctus elit. Nunc sit amet massa varius est sodales eleifend sit amet et quam. Fusce ac scelerisque magna. Pellentesque porttitor rutrum neque vel fermentum.  Nam sem quam, ultrices quis aliquam in, varius a neque. Fusce elit leo, consectetur in eleifend eget, facilisis at mi. Etiam porttitor gravida ipsum et sollicitudin. Integer magna mauris, bibendum ac porttitor interdum, mattis non libero. Duis placerat felis in pellentesque dignissim. Sed vitae rhoncus purus, sit amet pulvinar magna. Donec aliquam efficitur eros, eu mattis purus porttitor sed. Fusce neque elit, imperdiet vel eleifend ut, ornare vitae lectus. Maecenas tempus tortor a ante scelerisque pellentesque. Suspendisse ac turpis at nisi dictum rutrum ac nec nisi. Sed non ipsum ac nunc venenatis placerat."
 	box := makeBox("grimmwa.re", ridonk, 1, 2, h-3, w-3)
 	writeln(":PRESS C-q TO EXIT", 0, 0)
 	drawBox(box)
