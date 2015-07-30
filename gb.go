4266d2d9d2af4426125e352f701e95ec56e1f2d3
Matt Carroll <mattc@yelp.com>
2015-07-30 13:05:12 +0100

    Box line colours are now programmable

--- gb.go
+++ gb.go
@@ -15,23 +15,18 @@ type Coord struct {
 }
 
 type Box struct {
-	Title string
-	Color ui.Attribute
-	TL    Coord // top left
-	TR    Coord // top right
-	BL    Coord // bottom left
-	BR    Coord // bottom right
-	W     int
-	H     int
-	Text  string
+	Title     string
+	LineColor ui.Attribute
+	TL        Coord // top left
+	TR        Coord // top right
+	BL        Coord // bottom left
+	BR        Coord // bottom right
+	W         int
+	H         int
+	Text      string
 }
 
-func on_exit() {
-	fmt.Printf("thanks for playing\n")
-	fmt.Println(strings.Join(debug, "||"))
-}
-
-func makeBox(title, content string, x, y, height, width int) Box {
+func makeBox(title, content string, x, y, height, width int, linecolor ui.Attribute) Box {
 	var box Box
 	box.Title = title
 	box.TL.X = x
@@ -45,25 +40,26 @@ func makeBox(title, content string, x, y, height, width int) Box {
 	box.W = width
 	box.H = height
 	box.Text = content
+	box.LineColor = linecolor
 
 	return box
 }
 
 func drawBoxCorners(box Box) {
-	ui.SetCell(box.TL.X, box.TL.Y, '┌', f, d)
-	ui.SetCell(box.TR.X, box.TR.Y, '┐', f, d)
-	ui.SetCell(box.BL.X, box.BL.Y, '└', f, d)
-	ui.SetCell(box.BR.X, box.BR.Y, '┘', f, d)
+	ui.SetCell(box.TL.X, box.TL.Y, '┌', box.LineColor, d)
+	ui.SetCell(box.TR.X, box.TR.Y, '┐', box.LineColor, d)
+	ui.SetCell(box.BL.X, box.BL.Y, '└', box.LineColor, d)
+	ui.SetCell(box.BR.X, box.BR.Y, '┘', box.LineColor, d)
 }
 
 func drawBoxSides(box Box) {
 	for i := box.TL.X + 1; i < box.TR.X; i++ {
-		ui.SetCell(i, box.TL.Y, '─', f, d)
-		ui.SetCell(i, box.BL.Y, '─', f, d)
+		ui.SetCell(i, box.TL.Y, '─', box.LineColor, d)
+		ui.SetCell(i, box.BL.Y, '─', box.LineColor, d)
 	}
 	for j := box.TL.Y + 1; j < box.BL.Y; j++ {
-		ui.SetCell(box.TL.X, j, '│', f, d)
-		ui.SetCell(box.BR.X, j, '│', f, d)
+		ui.SetCell(box.TL.X, j, '│', box.LineColor, d)
+		ui.SetCell(box.BR.X, j, '│', box.LineColor, d)
 	}
 }
 
@@ -110,6 +106,11 @@ func writeln(s string, x, y int) {
 	}
 }
 
+func on_exit() {
+	fmt.Printf("thanks for playing\n")
+	fmt.Println(strings.Join(debug, "||"))
+}
+
 func main() {
 	defer on_exit()
 	err := ui.Init()
@@ -123,9 +124,9 @@ func main() {
 	// ui.SetInputMode(ui.InputEsc) //default
 	w, h := ui.Size()
 	ridonk := " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae mollis eros. Aenean at nisl laoreet, porta risus in, placerat dolor. Etiam massa augue, consequat efficitur augue pellentesque, viverra facilisis ex. Curabitur tristique nulla eget urna semper vestibulum. Cras eget eros ex. Vestibulum at ante leo. Quisque faucibus vulputate lacus vitae porta. Vivamus nulla erat, elementum id pulvinar sed, dapibus non diam. Nulla varius finibus est, ut laoreet enim venenatis id. Aliquam suscipit lectus neque, sit amet sollicitudin dui accumsan nec. Nulla enim felis, molestie at facilisis vitae, elementum et mauris. Aenean sit amet finibus ligula, ut luctus elit. Nunc sit amet massa varius est sodales eleifend sit amet et quam. Fusce ac scelerisque magna. Pellentesque porttitor rutrum neque vel fermentum.  Nam sem quam, ultrices quis aliquam in, varius a neque. Fusce elit leo, consectetur in eleifend eget, facilisis at mi. Etiam porttitor gravida ipsum et sollicitudin. Integer magna mauris, bibendum ac porttitor interdum, mattis non libero. Duis placerat felis in pellentesque dignissim. Sed vitae rhoncus purus, sit amet pulvinar magna. Donec aliquam efficitur eros, eu mattis purus porttitor sed. Fusce neque elit, imperdiet vel eleifend ut, ornare vitae lectus. Maecenas tempus tortor a ante scelerisque pellentesque. Suspendisse ac turpis at nisi dictum rutrum ac nec nisi. Sed non ipsum ac nunc venenatis placerat."
-	box := makeBox("grimmwa.re", ridonk, 1, 2, h-3, w-3)
+	box := makeBox("grimmwa.re", ridonk, 1, 2, h-3, w-3, orange)
 	someMsg := "lots of words and stuff"
-	box2 := makeBox("stuff", someMsg, 4, 5, 3, len(someMsg)+2)
+	box2 := makeBox("stuff", someMsg, 4, 5, 3, len(someMsg)+2, ui.ColorDefault)
 	writeln(":PRESS C-q TO EXIT", 0, 0)
 	drawBox(box)
 	drawBox(box2)
